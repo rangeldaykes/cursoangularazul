edb3c1ba2dc00595a5fa85303efe6b4c1d3c8830
rangel <daykes@gmail.com>
2019-06-05 13:41:22 -0300

    meat app

---
+++ meat-app-starter/src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Restaurant } from './restaurant/restaurant.model'
+import { RestaurantsService } from './restaurants.service';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[]
+
+  constructor(private restaurantsService: RestaurantsService) { }
+
+  ngOnInit() {
+    console.log("vai buscar");
+
+    this.restaurantsService.restaurants()
+      .subscribe(restaurants => this.restaurants = restaurants)
+
+    console.log("buscou");
+  }
+
+}
