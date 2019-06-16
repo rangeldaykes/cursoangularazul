1c898cf446d377a239b55c59a985cdae1a46da66
rangel <daykes@gmail.com>
2019-06-16 20:42:43 -0300

    menu item, shopping cart

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -15,7 +15,9 @@ import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 
 @NgModule({
   declarations: [
@@ -28,14 +30,15 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
     RestaurantDetailComponent,
     MenuComponent,
     ShoppingCartComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -9,6 +9,7 @@ import { Restaurant } from "./restaurant/restaurant.model";
 
 import { MEAT_API } from "../app.api";
 import { ErrorHandler } from "../app.error-handler";
+import { MenuItem } from "../restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -22,8 +23,23 @@ export class RestaurantsService {
   }
 
   restaurantById(id: string): Observable<Restaurant> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}`)
       .map(response => response.json())
-      .catch(ErrorHandler.handleError)
+      .catch(ErrorHandler.handleError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(resp => resp.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]>{
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}/menu`)
+      .map(resp => resp.json())
+      .catch(ErrorHandler.handleError);
   }
 }
